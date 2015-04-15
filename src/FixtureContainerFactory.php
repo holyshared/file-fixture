1691ace5195cf1f8746bd27aefdba08a03dcc63f
holyshared <holy.shared.design@gmail.com>
2015-04-15 13:12:17 +0900

    REfactaring

--- src/FixtureContainerFactory.php
+++ src/FixtureContainerFactory.php
@@ -5,41 +5,16 @@ namespace holyshared\fixture\file;
 class FixtureContainerFactory
 {
 
-    private $container;
-    private $currentPath;
-
-
-    public function __construct()
-    {
-        $this->container = [];
-        $this->currentPath = new PathSegment;
-    }
-
     public function createFromFile($configFile)
     {
     }
 
     public function createFromArray(array $configValues = [])
     {
-        $this->walkDictionary($configValues);
+        $flattener = new ArrayFlattener();
+        $flattenArray = $flattener->flatten($configValues);
 
-        return new FixtureContainer($this->container);
-    }
-
-    private function walkDictionary(array $configValues = [])
-    {
-        foreach ($configValues as $strategy => $value) {
-            $this->currentPath->moveTo($strategy);
-
-            if (is_array($value)) {
-                $this->walkDictionary($value);
-            } else {
-                $key = (string) $this->currentPath;
-                $this->container[$key] = $value;
-            }
-
-            $this->currentPath->moveParent();
-        }
+        return new FixtureContainer($flattenArray);
     }
 
 }
