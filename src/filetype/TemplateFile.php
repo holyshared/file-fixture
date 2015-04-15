003f679b9085c84c9f9e3f8a3cc979908353ca21
holyshared <holy.shared.design@gmail.com>
2015-04-15 14:00:12 +0900

    Add template file

--- src/filetype/TemplateFile.php
+++ src/filetype/TemplateFile.php
@@ -4,12 +4,24 @@ namespace holyshared\fixture\file\filetype;
 
 use holyshared\fixture\file\FileLoadable;
 use holyshared\fixture\file\FixtureFile;
+use Mustache_Engine;
+
 
 final class TemplateFile extends FixtureFile implements FileLoadable
 {
 
+    public function __construct($path)
+    {
+        $this->mustache = new Mustache_Engine;
+        parent::__construct($path);
+    }
+
     public function load(array $arguments = [])
     {
+        $path = $this->getPath();
+        $template = file_get_contents($this->getPath());
+
+        return $this->mustache->render($template, $arguments);
     }
 
 }
