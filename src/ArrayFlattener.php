73db0403098271dc038e27fdd63899695aa13af5
holyshared <holy.shared.design@gmail.com>
2015-04-16 09:20:57 +0900

    Add file headers

--- src/ArrayFlattener.php
+++ src/ArrayFlattener.php
@@ -2,6 +2,14 @@
 
 namespace holyshared\fixture\file;
 
+/**
+ * This file is part of file-fixture.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 class ArrayFlattener
 {
