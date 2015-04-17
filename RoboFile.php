46854ec49086fffe15709daa9ee2e7173882bdd9
holyshared <holy.shared.design@gmail.com>
2015-04-17 09:32:15 +0900

    Add coveralls config

---
+++ RoboFile.php
@@ -0,0 +1,19 @@
+<?php
+
+use \Robo\Tasks;
+use \coverallskit\robo\loadTasks as CoverallsKitTasks;
+
+
+class RoboFile extends Tasks
+{
+    use CoverallsKitTasks;
+
+    public function coverallsUpload()
+    {
+        $result = $this->taskCoverallsKit()
+            ->configureBy('.coveralls.toml')
+            ->run();
+
+        return $result;
+    }
+}
